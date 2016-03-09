9af504de51b1c82fcc8d7cd3a6167f0cbddd728c
elcoruco <el.coruco@gmail.com>
2016-03-09 13:40:20 -0600

    the categories list complete

---
+++ public/js/categories.js
@@ -0,0 +1,63 @@
+[{
+  "name" : "Educación",
+  "sub"  : ["Títulos","Cédulas","Certificados","Becas","Libros","Indígenas","Deporte","Tecnología"],
+  "tags" : ["Prescolar", "primaria", "secundaria", "preparatoria", "técnica", "adultos", "hombres", "mujeres"]
+},
+{
+  "name" : "Salud",
+  "sub"  : ["IMSS","Hospitales","Centros","Doctores","Epidemias","Infecciones","Vacunas","Higiene","Embarazo","Animales"],
+  "tags" : ["Niños", "niñas", "jóvenes", "adultos", "adultos mayores", "hombres", "mujeres"]
+},
+{
+  "name" : "Gobernación",
+  "sub"  : ["Violencia","Delitos","Sociedad Civil","Derechos Humanos","Participación","Radio","Televisión"],
+  "tags" : ["Niños", "niñas", "jóvenes", "adultos", "adultos mayores", "hombres", "mujeres"]
+},
+{
+  "name" : "Relaciones Exteriores",
+  "sub"  : ["Pasaportes","Visas","Naturalización","Viajes","Migración"],
+  "tags" : ["Niños", "niñas", "jóvenes", "adultos", "adultos mayores", "hombres", "mujeres"]
+},
+{
+  "name" : "Hacienda y Crédito Público",
+  "sub"  : ["Impuestos","Declaraciones","Devoluciones","Firma","Factura","Registro"],
+  "tags" : ["Hombres", "mujeres", "empresas", "negocios"]
+},
+{
+  "name" : "Desarrollo Social",
+  "sub"  : ["Pensiones","Jefas de familia","Empleo","Estancias","Migrantes","Indígenas"],
+  "tags" : ["Niños", "niñas", "jóvenes", "adultos", "adultos mayores", "hombres", "mujeres"]
+},
+{
+  "name" : "Medio Ambiente",
+  "sub"  : ["Suelos","Bosques","Áreas naturales","Animales","Permisos","Licencias","Autorizaciones","Minería","Contaminación"],
+  "tags" : ["Niños", "niñas", "jóvenes", "adultos", "adultos mayores", "hombres", "mujeres", "empresas", "negocios"]
+},
+{
+  "name" : "Energía",
+  "sub"  : ["Electricidad","Luz","Petróleo","Permisos","Pagos","Reportes"],
+  "tags" : ["Hombres", "mujeres", "empresas", "negocios"]
+},{
+  "name" : "Economía",
+  "sub"  : ["Importación","Exportación","Permisos","Registro","Apoyos","Consumidor","Créditos","Industria","Agricultura","Ganadería","Pesca","Campo"],
+  "tags" : ["Hombres", "mujeres", "empresas", "negocios"]
+},
+{
+  "name" : "Comunicaciones y Transportes",
+  "sub"  : ["Carreteras","Aeropuertos","Caminos","Empleo","Televisión"],
+  "tags" : ["Niños", "niñas", "jóvenes", "adultos", "adultos mayores", "hombres", "mujeres", "empresas", "negocios"]
+},
+{
+  "name" : "Trabajo",
+  "sub"  : ["Empleo","Capacitacion","Sindicatos","Beneficios","Derechos","Patrones","IMSS","ISSSTE","Pensiones","Préstamos"],
+  "tags" : ["jóvenes", "adultos", "adultos mayores", "hombres", "mujeres", "empresas", "negocios"]
+},
+{
+  "name" : "Desarrollo Territorial",
+  "sub"  : ["Vivienda","Desarrollo","Créditos","Urbanización","Propiedad","Rural","Urbano"],
+  "tags" : ["Niños", "niñas", "jóvenes", "adultos", "adultos mayores", "hombres", "mujeres", "empresas", "negocios"]
+},{
+  "name" : "Turismo",
+  "sub"  : ["Turistas","Guías","Pueblos Mágicos","Mercados","Viajes","Créditos"],
+  "tags" : ["Niños", "niñas", "jóvenes", "adultos", "adultos mayores", "hombres", "mujeres", "empresas", "negocios"]
+}];
