9e36e628b178bb92678b125ce5d9ceddb713900a
hugovom <hugo.om@gmail.com>
2016-03-09 13:39:03 -0600

    uptade results page in frontend

--- resources/views/frontend/results.blade.php
+++ resources/views/frontend/results.blade.php
@@ -7,7 +7,34 @@
 			<div class="col-sm-8 col-sm-offset-2">
 				<h1>Resultados de cuestionarios en <strong>Tú Evalúas</strong></h1>
 				<section>
-					<h2>Aún no hay encuestas para descargar</h2>
+					@if ($surveys->count() > 0)
+						@foreach($surveys as $survey)
+							<h2><a href="{{ url('resultados/'. $survey->id)}}">{{ $survey->title}}</h2>
+							<?php 
+								switch($survey->id){
+										case 1:
+											$figure = "prospera.jpg";
+											break;
+										case 2: 
+											$figure = "inadem.jpg";
+											break;
+										default:
+											$figure = "default.jpg";
+								}?>
+							
+							<a href="{{url('resultados/'. $survey->id) }}">
+								<figure>
+									<img src="{{url('img/programas/'.$figure)}}">
+								</figure>
+							</a>
+							<p class="lead">
+								<!-- aquí se agregará la descripción de cada encuesta-->
+								<a href="{{ url('resultados/'.$survey->id)}}" class="btn"> Consulta los resultados</a>
+							</p>
+						@endforeach
+					@else 
+						<h2>Aún no hay encuestas para descargar</h2>
+					@endif
 				</section>
 			</div>			
 		</article>
