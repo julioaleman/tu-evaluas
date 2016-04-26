2d77aa666c9333ccc1a2baa48d4caba53329cad9
hugovom <hugo.om@gmail.com>
2016-04-25 21:32:38 -0500

    update home w Julio files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,51 +1,59 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="container">
+
+<div class="container.vertical-buffer">
   <header class="row">
     <div class="col-sm-8 col-sm-offset-2">
-      <h1 class="tuevaluas">Tú Evalúas</h1>
-      <h2 class="intro">Tu opinión sobre los programas públicos federales ayuda a mejorarlos.</h2>
-      <p>Si recibes un correo con la invitación,<br>
-        ¡<strong>participa</strong>, eres muy importante!</p>
+	    <hr>
+      	<div class="contenedor image.vertical-buffer" align="center">
+        	<img src="{{ url('img/logov0.png') }}" width="750"></image>
+    	</div>
+<h2 align="center" class="intro">Tu opinión sobre los programas públicos federales ayuda a mejorarlos.<br><br></h2>
+      <h4 align="center">Si recibes un correo con la invitación,<br>
+        ¡<strong>participa</strong>, eres muy importante!<br><br><br></h4>
     </div>
   </header>
-  <section class="programs row">
-    @if($surveys->count() > 0)
-    <div class="col-sm-10 col-sm-offset-1">
-    <h2>Programas Evaluados</h2>
+  <section class="programs">
+	  <div class="container">
+	    <div class="row">
+	    @if($surveys->count() > 0)
+	    <hr>
+	    	<div class="col-sm-8 col-sm-offset-2">
+				<h3>Programas Evaluados</h3>
+				<hr class="red">
+	    	</div>
+	    	<div class="col-sm-8 col-sm-offset-2">
+				<ul>
+					@foreach($surveys as $survey)
+					  <li> <a href="{{url('resultado/' . $survey->id)}}">{{$survey->title}} <strong>&gt;</strong></a> </li>
+					@endforeach
+				</ul>
+	    	</div>
+    	@endif
+        <div class="col-sm-8 col-sm-offset-2">
+        <h3 class="intro">¿Qué es <strong>Tú Evalúas</strong>?</h3>
+        <hr class="red">
+        <p><strong>Tú Evalúas</strong> es una plataforma digital que <strong>tiene como objetivo evaluar el desempeño</strong> de los programas públicos 
+          federales mediante la participación ciudadana. A través de <strong>Tú Evalúas</strong> podrás calificar los procesos seguidos 
+          por cada programa y expresar tu 
+          satisfacción con los productos y servicios que ofrecen.</p>
+        <p><strong>Tú Evalúas</strong> facilitará la colaboración entre el gobierno y la ciudadanía partiendo de una base 
+          tecnológica para:</p>
+        <ol>
+          <li>Conocer la opinión ciudadana sobre la operación de los programas                      </li>
+          <li>Mejorar la evaluación de los programas con base en las necesidades expresadas directamente por la ciudadanía</li>
+          <li>Generar espacios de aprendizaje que conduzcan a mejores políticas públicas.                 </li>
+        </ol>
+        <p>La meta es que <strong>Tú Evalúas</strong> reúna toda la información referente a la satisfacción de la ciudadanía 
+          con respecto a los programas públicos federales para que quien lo desee (ciudadanos, 
+          asociaciones, gobierno o la comunidad en conjunto) pueda dar seguimiento al pulso ciudadano.
+        </p>
+        <br>
+        <br>
     </div>
-    @endif
-    <div class="col-sm-8 col-sm-offset-2">
-    <ul>
-    @foreach($surveys as $survey)
-      <li class="row">
-        <div class="col-sm-8">
-          <a href="{{url('resultado/' . $survey->id)}}">{{$survey->title}} <strong>&gt;</strong></a>
-        </div>
-        <div class="col-sm-4">
-          <?php
-            /*
-            // revisa si existe el CSV
-            $csv  = $survey->csv_file;
-            $path = $csv ? $csv_path . $csv : false;
-            $file = $path ? get_file_info($path) : false;
-            $url  = $file && $file['size'] ? "/csv/{$csv}" : false;
-          ?>
-          <?php if($url): ?>
-          <!-- si existe el CSV, muestra el link -->
-          <span class="data">
-            <a href="<?php echo $url; ?>">Descargar datos</a>
-            </span>
-          <span class="date"><?php echo date('d-m-Y h:iA', $file['date']); ?></span>
-        <?php endif; ?>
-        */ ?>
-
-        </div>
-      </li>
-    @endforeach
-    </ul>
-    </div>
+    	</div>
+    	</div>
   </section>
 </div>
 @endsection
