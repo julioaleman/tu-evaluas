3fb873a77d1c8648ded135a7cf5c29e2e492c075
el coruco <el.coruco@gmail.com>
2016-05-02 01:26:58 -0500

    add description field to blueprints

---
+++ database/migrations/2016_05_02_062526_add_description_field_to_blueprints_tabe.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDescriptionFieldToBlueprintsTabe extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('blueprints', function (Blueprint $table) {
+          $table->text("description")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('blueprints', function (Blueprint $table) {
+            //
+        });
+    }
+}
