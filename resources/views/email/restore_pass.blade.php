7dc955147ddf951264dadafa5abeaa2137deae45
hugovom <hugo.om@gmail.com>
2016-05-06 01:19:12 -0500

    fix #123

--- resources/views/email/restore_pass.blade.php
+++ resources/views/email/restore_pass.blade.php
@@ -7,11 +7,13 @@
 
 <body>
 <!-- saludo -->
-<h2 style="color:#626C8C; font: 500 24px sans-serif; margin:20px auto; padding: 0px 20px; background: #fff;  text-align: center;">
-Hola, da clic en el siguiente link para cambiar tu contraseña</h2>
-<p><a href="{{ url('password/' . $pass_key) }}">Recuperar contraseña</a></p>
-<p>Este link será válido durante 3 días.</p>
-</ul>
+<h1 style="font-family: 'Open Sans Light', Helvetica, Arial, sans-serif ;color:#545454; font-size:38px; line-height:1.428; text-align:left; padding-top: 25px; padding-bottom: 2.5px;">Hola, da clic en el siguiente link para cambiar tu contraseña</h1> 
+<hr style="border-top: 1px solid #DDDDDD; border-bottom:none; border-left: 1px solid #D0021B;; border-right:none; height: 6px; border-left-width: 35px"> 
+<p style="font-family: 'Open Sans Light', Helvetica, Arial, sans-serif;color:#545454; font-size:16px; line-height:1.428; text-align:justify"> 
+<a href="{{ url('password/' . $pass_key) }}" style="color:#12C">Recuperar contraseña</a> </p> 
+
+<p style="font-family: 'Open Sans Light', Helvetica, Arial, sans-serif ;color:#545454; font-size:16px; line-height:1.428; text-align:justify"> Este link será válido durante 3 días. </p>
 
 </body>
 </html>
+
