e6e61acd473b9d922671c4361780795660486921
hugovom <hugo.om@gmail.com>
2016-05-06 01:40:27 -0500

    fix #113

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,13 +3,13 @@
 	<div class="col-md-12">		
 		<h3>Equipo <strong>Tú evalúas</strong></h3>
 		<div class="row">
-		   <p class="col-xs-4"><img src="{{ url('img/presidencia_.png') }}"></p>
-	        <p class="col-xs-4"><a href="http://www.transparenciapresupuestaria.gob.mx"><img src="{{ url('img/transparencia_presupuestaria_.png') }}"></a></p>
-		   <p class="col-xs-4"><img src="{{ url('img/shcp_.png') }}"></p>
+		   <p class="col-xs-4"><a href="http://www.gob.mx/presidencia/"><img src="{{ url('img/presidencia_.png') }}"><br>Presidencia</a></p>
+	        <p class="col-xs-4"><a href="http://www.transparenciapresupuestaria.gob.mx"><img src="{{ url('img/transparencia_presupuestaria_.png') }}"><br>Transparencia<br>Presupuestaria</a></p>
+		   <p class="col-xs-4"><a href="http://www.gob.mx/hacienda"><img src="{{ url('img/shcp_.png') }}"><br>SHCP</a></p>
 		</div>
 		<div class="row">
-		   <p class="col-xs-4 col-xs-offset-2"><a href="http://wwwcrea.org.mx"><img src="{{ url('img/crea_emp_.png') }}"></a></p>
-		   <p class="col-xs-4"><a href="http://www.gobiernofacil.com"><img src="{{ url('img/gobierno_facil_.png') }}"></a><br><br></p>
+		   <p class="col-xs-4 col-xs-offset-2"><a href="http://www.crea.org.mx"><img src="{{ url('img/crea_emp_.png') }}"><br>CREA</a></p>
+		   <p class="col-xs-4"><a href="http://www.gobiernofacil.com"><img src="{{ url('img/gobierno_facil_.png') }}"><br>Gobierno Fácil</a></p>
 		</div>
 	</div>
 </div>
@@ -19,9 +19,13 @@
 	<div class="col-md-12">
 		<h3>Participantes <strong>Tú evalúas</strong></h3>
 		<div class="row">
-			<p class="col-xs-4"><a href="http://www.prospera.gob.mx"><img src="{{ url('img/sedesol_.png') }}"></a></p>
-            <p class="col-xs-4"><a href="http://www.prospera.gob.mx"><img src="{{ url('img/prospera_.png') }}"></a></p>
-			<p class="col-xs-4"><a href="http://www.inadem.gob.mx"><img src="{{ url('img/inadem_.png') }}"></a></p>
+			<p class="col-xs-4"><a href="http://www.prospera.gob.mx"><img src="{{ url('img/sedesol_.png') }}"><br>SEDESOL</a></p>
+            <p class="col-xs-4"><a href="http://www.prospera.gob.mx"><img src="{{ url('img/prospera_.png') }}"><br>PROSPERA</a></p>
+			<p class="col-xs-4"><a href="http://www.inadem.gob.mx"><img src="{{ url('img/inadem_.png') }}"><br>INADEM</a></p>
+		</div>
+		<div class="row">
+		   <p class="col-xs-4 col-xs-offset-2"><a href="http://www.imjuventud.gob.mx/"><img src="{{ url('img/imjuve_.png') }}"><br>IMJUVE</a></p>
+		   <p class="col-xs-4"><a href="http://www.conacyt.mx/"><img src="{{ url('img/conacyt_.png') }}"><br>CONACYT</a></p>
 		</div>
 	</div>
 </div>
