3294a02407526a059daea222121b517377edc96a
hugovom <hugo.om@gmail.com>
2016-05-17 12:51:09 -0500

    fix #141

--- resources/views/frontend/open-data.blade.php
+++ resources/views/frontend/open-data.blade.php
@@ -12,32 +12,41 @@
 	  </ol>
 	</div>
 </div>
-				<div class="bottom-buffer">
+<div class="container vertical-buffer">
 	<div class="col-md-8">
 		<h2>Datos Abiertos</h2>
 		<hr class="red">
-
+	</div>
+	<div class="col-md-8">
         @if ($surveys->count() > 0)
-          @foreach($surveys as $survey)
-    	  <h3><a href="{{ url('resultado/'. $survey->id)}}">{{ $survey->title}}</a></h3>
-    	  <p>
-    	    <a href="{{url('csv/' . $survey->csv_file . '.xlsx')}}" class="btn btn-primary btn-sm">XLSX</a>
-    	    <a href="{{url('csv/' . $survey->csv_file . '.csv')}}" class="btn btn-primary btn-sm">CSV</a>
-    	  </p>
-    	  <div class="row">
-			<div class="col-md-8">
-				<a href="{{$survey->ptp}}" class="btn btn-primary">Conoce el desempeño del programa presupuestario</a>
-			</div>
-		  </div>
+        <table class="table table-striped">
+            <thead clas="thead-default">
+              <tr>
+                <th>Nombre</th>
+                <th>Categoría</th>
+                <th>Descarga</th>
+              </tr>
+            </thead>
+			<tbody>
+			@foreach($surveys as $survey)
+          	<tr onclick="location='{{ url('resultado/'. $survey->id)}}'">
+	          	<td>{{ $survey->title}}</td>
+			  	<td>{{ $survey->category }}</td>
+			  	<td>
+				  	<a href="{{url('csv/' . $survey->csv_file . '.xlsx')}}" class="btn btn-link btn-sm">XLSX</a>
+				  	<a href="{{url('csv/' . $survey->csv_file . '.csv')}}" class="btn btn-link btn-sm">CSV</a>
+			  	</td>
+          	</tr>
   		  @endforeach
-  		  
-  		  <ul id="pagination">
+          </tbody>
+  		  <ul id="pagination" class="pagination">
             @for($i = 1; $i <= $pages; $i++)
             <li>
               <a href="{{url('datos-abiertos/' . $i) . '?' . http_build_query($request->all())}}" {{$page == $i ? 'class="current"' : ''}}>{{$i}}</a>
             </li>
             @endfor
           </ul>
+        </table>
         @else
         <p>Aún no hay datos abiertos disponibles :(</p>
         @endif
