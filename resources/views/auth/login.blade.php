ce87e0887d667039f975c203a453ac339d120f01
hugovom <hugo.om@gmail.com>
2016-05-23 11:39:43 -0500

    fix  #158

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,18 +4,25 @@
     <meta charset="utf-8">
     <title>Pase usted</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="{{url('img/favicon.ico')}}">
-  <link rel="stylesheet" href="{{url('css/normalize.css')}}">
-    <link rel="stylesheet" href="{{url('css/styles.css')}}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width"> 
+    <!-- Codigo GOB.MX CSS --> <link href="https://framework-gb.cdn.gob.mx/favicon.ico" rel="shortcut icon"> 
+    <link href="https://framework-gb.cdn.gob.mx/assets/styles/main.css" rel="stylesheet"> 
+    
   </head>
   <body class="login">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-8 col-sm-offset-2">
-        <h1 class="tu_evaluas">Tú Evalúas</h1>
-      </div>
-        <div class="col-sm-6 col-sm-offset-3">
-          <form name="nock-nock" method="post" action="{{ url('/login') }}">
+<main role="main"> <!--content--> 
+	  <div class="video-banner" style="width: 100%; max-width: 100%; height: 300px; background: grey url('/img/bannerTuEvaluas_2.png') no-repeat center center; background-size: cover; padding: 0 !important;"> 
+	</div> 
+	
+<div class="container vertical-buffer"> 
+	<div class="col-md-8"> 
+		<div class="row"> 
+				<h2>Iniciar sesión</h2> 
+		</div>
+	</div> 
+	<div class="row">
+		<div class="col-md-8"> 
+          <form name="nock-nock" method="post" action="{{ url('/login') }}" class="form-horizontal" role="form">
           {!! csrf_field() !!}
             <!-- [ ERROR MESSAGE ] 
             <?php /*
@@ -26,15 +33,25 @@
             <?php endif; ?>
             */ ?>
             -->
-
-            <!-- [ THE EMAIL ] -->
-            <p><label>Email</label>
-              <input type="text" name="email" id="email" value="{{ old('email') }}"></p>
-            
+			<div class="form-group"> 
+				<label class="col-sm-3 control-label" for="email">Email:</label> 
+				<div class="col-sm-9"> 
+					<input type="email"  name="email" class="form-control" id="email" placeholder="Email" value="{{ old('email') }}" required> 
+				</div> 
+			</div>
+			<div class="form-group"> 
+					<label class="col-sm-3 control-label" for="password">Contraseña:</label> 
+					<div class="col-sm-9"> 
             <!-- [ THE PASSWORD ] -->
-            <p><label>Contraseña</label><input type="password" name="password" id="password"><br>
-              <a href="{{url('password/reset')}}">recuperar contraseña</a></p>
-            <p><input type="submit" value="acceder"></p>
+						<input type="password" class="form-control" name="password" id="password" placeholder="Contraseña" required> <br>
+						<a href="{{url('password/reset')}}">Recuperar contraseña</a> 
+					</div> 
+				</div> 
+				<div class="form-group"> 
+					<div class="col-sm-offset-3 col-sm-9"> 
+						<button class="btn btn-primary pull-right" type="submit">Iniciar sesión</button> 
+					</div> 
+				</div> 
           </form>
         </div>
       </div>
@@ -73,5 +90,9 @@
     this.submit();
   }
 </script>
-  </body>
-</html>
+</main> 
+<!--footer--> 
+<!-- JS --> 
+<script src="https://framework-gb.cdn.gob.mx/gobmx.js"></script> 
+</body> 
+</html> 
